939157511636ec307775b2a820330521c722e850
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-08-18 12:08:12 +0530

    Create array_shortcuts_python.py

---
+++ array_shortcuts_python.py
@@ -0,0 +1,13 @@
+    #0   1  2  3  4  5  6  7
+arr=[11,22,33,44,55,66,77,88]
+print(arr[2])
+print(arr[1:5])
+print(arr[4:7])
+print(arr[:5])
+print(arr[3:])
+print(arr[:])
+print(arr[1:7:1])
+print(arr[1:7:2])
+print(arr[::1])
+print(arr[::2])
+print(arr[::-1])
