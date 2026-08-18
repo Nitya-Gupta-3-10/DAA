4419368466eccb3f2240b328d719f2bb7484fe43
Nitya-Gupta-3-10 <nitya31007gupta@gmail.com>
2026-08-18 10:27:23 +0530

    Add Armstrong number verification logic
    Implement Armstrong number check in Python.

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+no=int(input())
+save=no
+count=0
+while no>0:
+    no=no//10
+    count=count+1
+no=save
+while no>0:
+    rem=no%10
+    sum=sum+(rem**count)
+    no=no//10
+if sum==save:
+    print("Armstrong")
+else:
+    print("Not Armstrong")
